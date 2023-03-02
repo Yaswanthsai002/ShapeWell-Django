03aa06582f8bb3f0f0215e3f9240801137aa6cd4
Kotagiri Yaswanth Sai <yaswanthsaikotagiri1@gmail.com>
2023-03-02 11:00:56 +0530

    update

--- app/pose_estimation.py
+++ app/pose_estimation.py
@@ -89,16 +89,16 @@ def pose_estimation(model, label_decoder, mp_holistic, mp_drawing):
 
                 # Draw the landmarks
                 # 2. Right hand
-                # mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                #                             mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
-                #                             mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=2)
-                #                             )
+                mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                            mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=2)
+                                            )
 
                 # 3. Left Hand
-                # mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                #                             mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
-                #                             mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=2)
-                #                             )
+                mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                            mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=2)
+                                            )
 
                 # 4. Pose Detections
                 mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
@@ -121,16 +121,16 @@ def pose_estimation(model, label_decoder, mp_holistic, mp_drawing):
 
                 # Draw the landmarks
                 # 2. Right hand
-                # mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                #                             mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
-                #                             mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2)
-                #                         )
+                mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                            mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2)
+                                        )
 
                 # 3. Left Hand
-                # mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                #                             mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
-                #                             mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2)
-                #                         )
+                mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                            mp_drawing.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=4),
+                                            mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2)
+                                        )
 
                 # 4. Pose Detections
                 mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
